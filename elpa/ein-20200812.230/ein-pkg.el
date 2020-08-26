465620ae41d0e197a57b988ce3ce525b145808f4
Kyohei-Prj <tatsukawa929@gmail.com>
2020-08-26 20:51:16 +0900

    init commit

---
+++ elpa/ein-20200812.230/ein-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ein" "20200812.230" "Emacs IPython Notebook"
+  '((emacs "25")
+    (websocket "20190620")
+    (anaphora "20180618")
+    (request "20200117")
+    (deferred "0.5")
+    (polymode "20190714")
+    (exec-path-from-shell "20191229")
+    (dash "2.13.0")
+    (with-editor "20200522"))
+  :commit "99a4718f50538a439a2a3f2011d1aa97246e259b")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
