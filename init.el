d1a9c8b9548c2351bab9d4f984aa2c8527837177
Kyohei-Prj <tatsukawa929@gmail.com>
2020-09-28 09:26:07 +0900

    font size

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 102 :width normal)))))
 
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
