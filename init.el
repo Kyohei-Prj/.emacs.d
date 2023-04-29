a73f82fc40f31e420d0ceedb334dc3e505519fd5
Kyohei-Prj <tatsukawaforjob@gmail.com>
2023-04-29 22:10:20 +0900

    add key bind for counsel projectile find file

--- init.el
+++ init.el
@@ -150,6 +150,7 @@
 
 (leaf counsel-projectile
   :ensure t
+  :bind(("C-x p C-f" . 'counsel-projectile-find-file))
   :config
   (counsel-projectile-mode))
 
@@ -182,4 +183,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ein:basecell-input-area-face ((t (:extend t :background "color-235")))))
