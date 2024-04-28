7e0b84f285a7bbab4358b992dc31bb5760c715eb
Kyohei-Prj <tatsukawaforjob@gmail.com>
2024-04-28 15:46:29 +0900

    add lsp booster

--- init.el
+++ init.el
@@ -159,6 +159,7 @@
   (company-idle-delay . 0.01))
 ;; add company-quickhelp
 
+
 (setenv "LSP_USE_PLISTS" "true")
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1048576 4))
@@ -268,6 +269,35 @@
           (lambda ()
             (local-set-key (kbd "C-c C-b") 'create-code-block)))
 
+;; lsp booster setup
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 
 (provide 'init)
